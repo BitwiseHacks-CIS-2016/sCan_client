7623d6f6da5df33f918fc690ea5509660dc98b75
Bolt108 <YOUR EMAIL ADDRESS>
2016-02-20 18:43:59 +0800

    Added device code

---
+++ Trashcan_Device/Trashcan_Device.ino
@@ -0,0 +1,88 @@
+#include <WiFi.h>
+#include <Wire.h>
+#include <BMA222.h>
+#include "SPI.h"
+
+#include <aJSON.h>
+#include "M2XStreamClient.h"
+
+char ssid[] = "<ssid>"; //  your network SSID (name)
+char pass[] = "<password>"; // your network password (use for WPA, or use as key for WEP)
+
+int status = WL_IDLE_STATUS;
+
+char deviceId[] = "<device id>"; // Feed you want to post to
+char m2xKey[] = "<api key>"; // Your M2X access key
+char streamName[] = "<stream name>"; // Stream you want to post to
+
+BMA222 mySensor;
+
+WiFiClient client;
+
+int port = 6;
+
+M2XStreamClient m2xClient(&client, m2xKey);
+
+void setup() {
+
+  Serial.begin(9600);
+//  pinMode(RED_LED, OUTPUT); 
+
+//  mySensor.begin();
+//  uint8_t chipID = mySensor.chipID();
+//  Serial.print("chipID: ");
+//  Serial.println(chipID);
+
+  delay(10);
+
+  // attempt to connect to Wifi network:
+  Serial.print("Attempting to connect to Network named: ");
+  // print the network name (SSID);
+  Serial.println(ssid); 
+  WiFi.begin(ssid, pass); // Use this if your wifi network requires a password
+  // WiFi.begin(ssid);    // Use this if your wifi network is unprotected.
+  while ( WiFi.status() != WL_CONNECTED) {
+    // print dots while we wait to connect
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println("\nConnect success!");
+  Serial.println("Waiting for an ip address");
+
+  while (WiFi.localIP() == INADDR_NONE) {
+    // print dots while we wait for an ip addresss
+    Serial.print(".");
+    delay(300);
+  }
+
+  Serial.println("\nIP Address obtained");
+
+  // you're connected now, so print out the status  
+  printWifiStatus();
+
+}
+
+void loop() {
+  int sensorValue = analogRead(port);
+  int voltage = sensorValue * (3.0/1023.0);
+  
+  int response;
+  if(voltage > 0) {
+     response = m2xClient.updateStreamValue(deviceId,streamName,false); 
+  }
+  else {
+     response = m2xClient.updateStreamValue(deviceId,streamName,true);
+  }
+}
+
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+}
+
